9a995e68f1d65f3b71b7032f1475bb3d909d6106
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-11-11 19:53:30 +0100

    Fix part1

--- 2024/4/part1.py
+++ 2024/4/part1.py
@@ -9,7 +9,8 @@ def read_input() -> input_type:
     with open("input", "r") as file:
         lines = []
         for line in file:
-            lines.append(list(line.strip()))
+            if line.strip():
+                lines.append(list(line.strip()))
         return np.array(lines)
 
 
@@ -36,14 +37,14 @@ def string_count(input: input_type, target: str):
                     count += 1
 
             # Horizontal right
-            if col + length < cols:
+            if col + length - 1 < cols:
                 # right = "".join(input[row, col : col + length])
                 right = "".join([input[row, col + i] for i in range(length)])
                 if right == target:
                     count += 1
 
             # Vertical down
-            if row + length < rows:
+            if row + length - 1 < rows:
                 # down = "".join(input[row : row + length, col])
                 down = "".join([input[row + i, col] for i in range(length)])
                 if down == target:
@@ -63,19 +64,19 @@ def string_count(input: input_type, target: str):
                     count += 1
 
             # Diagonal up right
-            if col + length < cols and row - length + 1 >= 0:
+            if col + length - 1 < cols and row - length + 1 >= 0:
                 up_right = "".join([input[row - i, col + i] for i in range(length)])
                 if up_right == target:
                     count += 1
 
             # Diagonal down right
-            if col + length < cols and row + length < rows:
+            if col + length - 1 < cols and row + length - 1 < rows:
                 down_right = "".join([input[row + i, col + i] for i in range(length)])
                 if down_right == target:
                     count += 1
 
             # Diagonal down left
-            if col - length + 1 >= 0 and row + length < rows:
+            if col - length + 1 >= 0 and row + length - 1 < rows:
                 down_left = "".join([input[row + i, col - i] for i in range(length)])
                 if down_left == target:
                     count += 1
@@ -85,7 +86,6 @@ def string_count(input: input_type, target: str):
 
 def main() -> None:
     print("Count of XMAS:", string_count(read_input(), "XMAS"))
-    print("2310 IS WRONG ANSWER! https://adventofcode.com/2024/day/4")
 
 
 if __name__ == "__main__":
