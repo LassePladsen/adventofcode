d837e34bdc148079577120fe5ef744d01cebd4f2
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-04 15:42:00 +0100

    part2 WIP

--- 2025/1/rust/part2.rs
+++ 2025/1/rust/part2.rs
@@ -1,25 +1,83 @@
-use input::read_cols;
-
 mod input;
 
-fn main() {
-    println!("hei");
-    let (col1, col2) = read_cols("../input");
+const DEBUG: bool = true;
 
-    let mut sum = 0;
-
-    // add to sum: num * how many times num is in col2
-    for num in &col1 {
-        let count: i32 = col2
-            .iter()
-            .filter(|x| *x == num)
-            .count()
-            .try_into()
-            .unwrap();
-        if count < 1 {
-            continue;
+macro_rules! debugprint {
+    ($($arg:tt)*) => {
+        if DEBUG {
+            println!($($arg)*);
         }
-        sum += count * num;
+    };
+}
+
+/// Password is the number of times the dial has rotated at or past zero
+fn parse_password(input: &String) -> usize {
+    const START_POS: isize = 50;
+    const MAX_POS: isize = 99;
+    const MIN_POS: isize = 0;
+
+    let mut hit_zero_count: usize = 0;
+    let mut this_turn_hit_zero_count: isize;
+    let mut pos: isize = START_POS;
+    let mut new_pos: isize = START_POS;
+    let mut direction: char;
+    let mut pos_diff: isize;
+    for line in input.lines() {
+        direction = line.chars().nth(0).unwrap();
+
+        debugprint!("LP direction: {:?}", direction);
+        pos_diff = line[1..].parse().expect("Could not parse pos_diff");
+
+        if direction == 'L' {
+            pos_diff *= -1
+        }
+
+        debugprint!("LP pos_diff: {:?}", pos_diff);
+
+        new_pos = pos + pos_diff;
+        debugprint!("LP new pos before resolving: {pos:?}");
+
+        // Modulo to resolve within bounds
+        new_pos = new_pos % (MAX_POS + 1);
+
+        // Wrap around 99 if negative
+        if new_pos < MIN_POS {
+            new_pos = MAX_POS + 1 + new_pos;
+        }
+        debugprint!("LP final new new_pos: {new_pos:?}");
+        if new_pos < MIN_POS || new_pos > MAX_POS {
+            panic!(
+                "Final new new_pos is outside bounds with new_pos={new_pos}, min={MIN_POS}, max={MAX_POS}"
+            );
+        }
+
+        this_turn_hit_zero_count = pos / (MAX_POS + 1);
+        println!("LP this_turn_hit_zero_count before resolving landing at zero: {this_turn_hit_zero_count:?}");
+        if this_turn_hit_zero_count < 0 {
+            panic!("This_turn_hit_zero_count can't be negative! got {this_turn_hit_zero_count}");
+        }
+        // If landed at zero exactly
+        if new_pos == 0 && pos != 0 {
+            this_turn_hit_zero_count += 1;
+        }
+        debugprint!("LP this_turn_hit_zero_count: {this_turn_hit_zero_count:?}\n");
+
+        hit_zero_count += this_turn_hit_zero_count as usize;
+        pos = new_pos
     }
-    println!("{}", sum)
+
+    hit_zero_count
+}
+
+fn main() {
+    let input = input::read_input("../demoinput.txt");
+
+    if DEBUG {
+        println!("Input is:");
+        input.lines().for_each(|x| println!("{x}"));
+        println!();
+    }
+
+    let password = parse_password(&input);
+    println!("The password is {password}");
 }
