59ab754d18c6a119268e43b2ef4d43d8da8dcd7a
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-05 13:37:45 +0100

    experiment on wild goose chase

--- 2025/2/rust/part1.rs
+++ 2025/2/rust/part1.rs
@@ -34,7 +34,7 @@ fn main() {
         for id in start..end + 1 {
             debugprint!("id is {id}");
 
-            if is_invalid(id) {
+            if invalid(id) {
                 debugprint!("Id {id} is invalid! Adding to sum");
                 invalid_sum += id;
                 debugprint!("LP new invalid_sum: {invalid_sum:?}");
@@ -46,21 +46,42 @@ fn main() {
     println!("Total sum of invalid ids: {invalid_sum}");
 }
 
-/// An id is invalid if it consists only of chars that are duplicated exactly once
-/// TODO: needs to check not only one character at a time, but possible substring.
+/// An id is invalid as long as it only consists of some sequence of digits repeated exactly once.
+/// TODO: needs to check not only one character at a time, but all possible substrings.
 /// i.e. 123123 is invalid cause its 123 twice
-fn is_invalid(id: u128) -> bool {
-    // Track count of every digit found
-    let mut digit_counts: HashMap<char, usize> = HashMap::new();
+/// Plan is to maybe store all combinations of substrings (all different lenghts possible) in a
+/// hashmap, create a count and somehow parse the hashmap to find if can be marked as invalid.
+fn invalid(id: u128) -> bool {
+    let s = id.to_string();
+    // Track count of a string slice in the id
+    type DigitMap<'a> = HashMap<&'a str, usize>;
+
+    // For all possible substring lengths, keep track of the substring count in this id
+    // Key is length of substring
+    let mut substr_maps: HashMap<usize, DigitMap> = HashMap::new();
+
+    // Loop over possible substring lengths
+    // TODO: we might only need to do up to and including half the length?
+    for strlen in 1..s.len() {
+        // Populate this substring length with all substrings
+        // Key is substring, value is the count
+        let mut substr_counts: DigitMap = HashMap::new();
+
+        for substr in s.splitn(strlen, substr) {
+            substr_counts.insert(substr, s.matches(substr).count());
+        }
+        substr_maps.insert(strlen, substr_counts);
+    }
 
     for c in id.to_string().chars() {
         let count = digit_counts.entry(c).or_insert(0);
         *count += 1;
 
-        // We know early this is valid if seen more than twice
+        // We know immediately this is valid if seen more than twice
         if *count > 2 {
             return false;
         }
+
         debugprint!("LP digit_counts: {digit_counts:?}");
     }
 
