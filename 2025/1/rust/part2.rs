efb478bc6997d338bb1d139ca7adf8b28be8aa77
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-05 09:00:51 +0100

    More robust part2, but still too low answer

--- 2025/1/rust/part2.rs
+++ 2025/1/rust/part2.rs
@@ -19,11 +19,14 @@ fn parse_password(input: &String) -> usize {
     let mut hit_zero_count: usize = 0;
     let mut this_turn_hit_zero_count: isize;
     let mut pos: isize = START_POS;
-    let mut new_pos: isize = START_POS;
+    let mut new_pos: isize;
+    let mut resolved_pos: isize;
     let mut direction: char;
     let mut pos_diff: isize;
     for line in input.lines() {
         direction = line.chars().nth(0).unwrap();
+
+        debugprint!("LP start pos: {pos}");
 
         debugprint!("LP direction: {:?}", direction);
         pos_diff = line[1..].parse().expect("Could not parse pos_diff");
@@ -35,48 +38,60 @@ fn parse_password(input: &String) -> usize {
         debugprint!("LP pos_diff: {:?}", pos_diff);
 
         new_pos = pos + pos_diff;
-        debugprint!("LP new pos before resolving: {pos:?}");
+        debugprint!("LP new pos before resolving: {new_pos:?}");
 
         // Modulo to resolve within bounds
-        new_pos = new_pos % (MAX_POS + 1);
+        resolved_pos = new_pos % (MAX_POS + 1);
 
         // Wrap around 99 if negative
-        if new_pos < MIN_POS {
-            new_pos = MAX_POS + 1 + new_pos;
+        if resolved_pos < MIN_POS {
+            resolved_pos = MAX_POS + 1 + resolved_pos;
         }
-        debugprint!("LP final new new_pos: {new_pos:?}");
-        if new_pos < MIN_POS || new_pos > MAX_POS {
+        if resolved_pos < MIN_POS || resolved_pos > MAX_POS {
             panic!(
                 "Final new new_pos is outside bounds with new_pos={new_pos}, min={MIN_POS}, max={MAX_POS}"
             );
         }
+        println!("LP resolved_pos: {resolved_pos:?}");
 
-        this_turn_hit_zero_count = pos / (MAX_POS + 1);
-        println!("LP this_turn_hit_zero_count before resolving landing at zero: {this_turn_hit_zero_count:?}");
+        // Number of times passed zero is the diff between new_pos before and after resolving back inside bounds, divided by 100.
+        // But we need to handle the edge case where we started/landed at exactly zero
+        this_turn_hit_zero_count = (resolved_pos - new_pos).abs() / (MAX_POS + 1);
+        println!(
+            "LP this_turn_hit_zero_count before resolving edge cases at zero: {this_turn_hit_zero_count:?}"
+        );
+
+        // If started at zero exactly, but didnt make it across the entire wheel back to
+        // zero
+        if pos == 0 && pos_diff < (MAX_POS + 1) && this_turn_hit_zero_count >= 1 {
+            this_turn_hit_zero_count -= 1;
+        }
+        // If landed at zero exactly
+        if resolved_pos == 0 && this_turn_hit_zero_count == 0 {
+            this_turn_hit_zero_count += 1;
+        }
+
         if this_turn_hit_zero_count < 0 {
             panic!("This_turn_hit_zero_count can't be negative! got {this_turn_hit_zero_count}");
         }
-        // If landed at zero exactly
-        if new_pos == 0 && pos != 0 {
-            this_turn_hit_zero_count += 1;
-        }
+
         debugprint!("LP this_turn_hit_zero_count: {this_turn_hit_zero_count:?}\n");
 
         hit_zero_count += this_turn_hit_zero_count as usize;
-        pos = new_pos
+        pos = resolved_pos;
     }
 
     hit_zero_count
 }
 
 fn main() {
-    let input = input::read_input("../demoinput.txt");
+    let input = input::read_input("../input.txt");
 
-    if DEBUG {
-        println!("Input is:");
-        input.lines().for_each(|x| println!("{x}"));
-        println!();
-    }
+    // if DEBUG {
+    //     println!("Input is:");
+    //     input.lines().for_each(|x| println!("{x}"));
+    //     println!();
+    // }
 
     let password = parse_password(&input);
     println!("The password is {password}");
