89b31556a928a137555308f26be57bec8bee7ec9
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-12-09 08:16:45 +0100

    did part1 in python, because fancy_regex crate in rust was wayy to slow

--- 2025/2/rust/part1.rs
+++ 2025/2/rust/part1.rs
@@ -1,8 +1,9 @@
 use fancy_regex::Regex;
+use rayon::prelude::*;
 
 mod input;
 
-const DEBUG: bool = true;
+const DEBUG: bool = false;
 
 macro_rules! debugprint {
     ($($arg:tt)*) => {
@@ -33,23 +34,36 @@ fn main() {
         debugprint!("The range is {start}-{end}");
 
         // Loop over every id in the range, check for invalids
-        for id in start..end + 1 {
-            debugprint!("id is {id}");
+        // for id in start..end + 1 {
+        //     debugprint!("id is {id}");
+        //     if !valid(id) {
+        //         debugprint!("Id {id} is invalid! Adding to sum");
+        //         invalid_sum += id;
+        //         debugprint!("LP new invalid_sum: {invalid_sum:?}");
+        //     }
+        //     debugprint!();
+        // }
+
+        // Above is SLOOOOW. Batch it into parallell threads instead
+        invalid_sum += (start..end+ 1).into_par_iter().map(|id| {
             if !valid(id) {
-                debugprint!("Id {id} is invalid! Adding to sum");
-                invalid_sum += id;
-                debugprint!("LP new invalid_sum: {invalid_sum:?}");
+                return id;
             }
-            debugprint!();
-        }
+            return 0;
+        }).sum::<u128>();
     }
 
     println!("Total sum of invalid ids: {invalid_sum}");
 }
 
 fn valid(id: u128) -> bool {
-    let re = Regex::new(r"^(\d+)\1$").expect("Invalid regex");
+    // save regex time by knowing its valid early if the string length is odd
     let s = id.to_string();
+    if s.len() % 2 == 2 {
+        return true;
+    }
+
+    let re = Regex::new(r"^(\d+)\1$").expect("Invalid regex");
     match re.captures(&s).expect("Error running regex") {
         Some(captures) => {
             debugprint!("{} captures: {captures:?}", captures.len());
